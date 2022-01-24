8c02e12c3ac11d9fe06a95837857be4782d72396
Al-Nahian Hasan <nahianhasan161@gmail.com>
2022-01-24 21:51:38 +0600

    maincontent Component Variable Problem fix 1.1(with())

--- resources/views/components/admin/partials/maincontent.blade.php
+++ resources/views/components/admin/partials/maincontent.blade.php
@@ -4,12 +4,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">{{$title}}</h1>
+            <h1 class="m-0">@if (isset($title)){{$title}}@else Title @endif</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">{{$root}}</a></li>
-              <li class="breadcrumb-item active">{{$child}}</li>
+              <li class="breadcrumb-item"><a href="#">@if (isset($root)){{$root}}@else Root @endif</a></li>
+              <li class="breadcrumb-item active">@if (isset($child)){{$child}}@else Child @endif</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
